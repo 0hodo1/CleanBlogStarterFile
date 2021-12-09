d9a97e626f0370b15b1b7bcae38fcfc6350a6ecb
Ömer Hodo <omerhodo@gmail.com>
2021-12-09 21:21:02 +0300

    cleanblog project part 2 completed

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// template engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 app.listen(port, () => {
