f1c88c8fc7bd97fd1ee303db51046b48c31f9737
Ömer Hodo <omerhodo@gmail.com>
2021-12-14 17:06:12 +0300

    model created and index page data listed

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const ejs = require('ejs');
+
+const Post = require('./models/Post');
 
 const app = express();
-const port = 3000;
+
+// connect db
+mongoose.connect('mongodb://127.0.0.1:27017/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // template engine
 app.set('view engine', 'ejs');
 
 // Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const post = await Post.find({});
+  res.render('index', {
+    post: post,
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
@@ -21,6 +36,12 @@ app.get('/post', (req, res) => {
 app.get('/add_post', (req, res) => {
   res.render('add_post');
 });
+app.post('/add_post', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
+
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} nolu portta çalışıyor...`);
